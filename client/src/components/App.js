7d562c24a231a41a3ef5d043d8ff9982e02e5832
Luqman Rasat <luqmanrasat@hotmail.com>
2019-08-13 20:00:50 +0800

    add auth

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,6 +3,7 @@ import { Switch, Route } from 'react-router-dom';
 import Header from './Header';
 import LinkList from './LinkList';
 import CreateLink from './CreateLink';
+import Login from './Login';
 
 class App extends Component {
   render() {
@@ -11,16 +12,9 @@ class App extends Component {
         <Header />
         <div className="ph3 pv1 background-gray">
           <Switch>
-            <Route 
-              exact
-              path="/"
-              component={ LinkList }
-            />
-            <Route
-              exact
-              path="/create"
-              component={ CreateLink }
-            />
+            <Route exact path="/" component={ LinkList }/>
+            <Route exact path="/create" component={ CreateLink }/>
+            <Route exact path="/login" component={ Login }/>
           </Switch>
         </div>
       </div>
