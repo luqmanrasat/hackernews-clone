5e3f380c5c77ca043fb1b3f42a0d54ead506ef92
Luqman Rasat <luqmanrasat@hotmail.com>
2019-08-13 16:35:33 +0800

    add header component. previous commit is update schema

--- client/src/components/CreateLink.js
+++ client/src/components/CreateLink.js
@@ -43,6 +43,7 @@ class CreateLink extends Component {
         <Mutation
           mutation={ POST_MUTATION }
           variables={{ description, url }}
+          onCompleted={ () => this.props.history.push('/') }
         >
           { postMutation => <button onClick={ postMutation }>Submit</button> }
         </Mutation>
