f6f82e3ac8bba392645a7e03eb917f176c5a75d8
Luqman Rasat <luqmanrasat@hotmail.com>
2019-08-14 17:29:43 +0800

    add search component

--- react-client/src/components/Header.js
+++ react-client/src/components/Header.js
@@ -11,11 +11,19 @@ class Header extends Component {
       <div className="flex pa1 justify-between nowrap orange">
         <div className="flex flex-fixed black">
           <div className="fw7 mr1">Hacker News</div>
-          <Link to="/" className="ml1 no-underline black">new</Link>
+          <Link to="/" className="ml1 no-underline black">
+            new
+          </Link>
+          <div className="ml1">|</div>
+          <Link to="/search" className="ml1 no-underline black">
+            search
+          </Link>
           {authToken && (
             <div className="flex">
               <div className="ml1">|</div>
-              <Link to="/create" className="ml1 no-underline black">submit</Link>
+              <Link to="/create" className="ml1 no-underline black">
+                submit
+              </Link>
             </div>
           )}
         </div>
@@ -31,7 +39,9 @@ class Header extends Component {
               logout
             </div>
           ) : (
-            <Link to="/login" className="ml1 no-underline black">login</Link>
+            <Link to="/login" className="ml1 no-underline black">
+              login
+            </Link>
           )}
         </div>
       </div>
